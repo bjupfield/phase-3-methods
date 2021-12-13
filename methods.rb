b6555ffc9241ca7591b731dcf27254565c4b1482
Bennett <bjupfield@gmail.com>
2021-12-13 10:35:51 -0600

    commit tree fe2036db96f14cac15a22e2d93eb0fcf5beff117

--- methods.rb
+++ methods.rb
@@ -1,1 +1,19 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+def greet(name = "No-One")
+    puts "Hello, #{name}!"
+end
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+def halve(num)
+    if(num.class != Integer)
+        return nil
+    end
+    num / 2
+end
+def add(num1, num2)
+    num1 + num2
+end
